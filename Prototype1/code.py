d813a3681765a883491604f54a00cab6bc831134
aW5mb2JlemRhcg <aW5mb2JlemRhcg@inbox.eu>
2026-03-15 16:14:03 +0300

    update codes

--- Prototype1/code.py
+++ Prototype1/code.py
@@ -1,6 +1,8 @@
 import board
 import displayio
 import terminalio
+import alarm
+import digitalio
 from adafruit_display_text import label
 from adafruit_ble import BLERadio
 from adafruit_ble.advertising.standard import Advertisement
@@ -8,13 +10,11 @@ import time
 
 display = board.DISPLAY
 ble = BLERadio()
-
-SCAN_TIME = 15
-MIN_SEEN_TIME = 8
-rssi_razb = 15
 skimmer_names = ["HC-05", "HC-06", "HM-10", "DX-BT18", "HC-03"]
+devices = {}
+founded = False
 
-def update_display(text, color=0x00FF00, scale=2, x=10, y=70):
+def update_display(text, color=0x00FF00, scale=2, x=10, y=70): #функция управления дисплеем
     group = displayio.Group()
     text_area = label.Label(
         terminalio.FONT,
@@ -30,10 +30,8 @@ def update_display(text, color=0x00FF00, scale=2, x=10, y=70):
 update_display("СКАНИРОВАНИЕ\nОЖИДАИТЕ", color=0x00FFDE, scale=3, x=20, y=50)
 time.sleep(3)
 
-devices = {}
-founded = False
-
-for adv in ble.start_scan(Advertisement, timeout=SCAN_TIME):
+#сканирование устройств
+for adv in ble.start_scan(Advertisement, timeout=15):
     address = str(adv.address)
     name = adv.complete_name or "Unknown"
     rssi = adv.rssi
@@ -41,8 +39,7 @@ for adv in ble.start_scan(Advertisement, timeout=SCAN_TIME):
     clean_name = name.rstrip('\x00').strip().upper()
     if any(skimmer.upper() in clean_name for skimmer in skimmer_names):
         founded = True
-        break 
-    
+        break
     if address not in devices:
         devices[address] = {
             "name": name,
@@ -53,6 +50,7 @@ for adv in ble.start_scan(Advertisement, timeout=SCAN_TIME):
     devices[address]["rssi"].append(rssi)
     devices[address]["last_seen"] = now
 
+#анализ RSSI
 if not founded:
     for data in devices.values():
         duration = data["last_seen"] - data["first_seen"]
@@ -61,16 +59,11 @@ if not founded:
             continue
         rssi_var = max(rssi_values) - min(rssi_values)
         avg_rssi = sum(rssi_values) / len(rssi_values)
-        
-        if (
-            data["name"] == "Unknown"
-            and duration >= MIN_SEEN_TIME
-            and rssi_var <= rssi_razb
-            and avg_rssi > -65
-        ):
+        if data["name"] == "Unknown" and duration >= 8 and rssi_var <= 15 and avg_rssi > -65:
             founded = True
             break
 
+#вывод результата
 if founded:
     update_display("ОПАСНОСТЬ!\nВОЗМОЖЕН\nСКИММЕР", color=0xC91616, scale=3, x=10, y=20)
 else:
@@ -78,5 +71,17 @@ else:
 
 print(devices)
 print(founded)
-while True:
-    time.sleep(1)
+#уход в сон
+time.sleep(60)
+display.root_group = displayio.Group()
+display.brightness = 0
+neo_power = digitalio.DigitalInOut(board.NEOPIXEL_POWER)
+neo_power.direction = digitalio.Direction.OUTPUT
+neo_power.value = False
+#ожидание нажатия кнопки
+button_alarm = alarm.pin.PinAlarm(
+    pin=board.BUTTON,
+    value=False,
+    pull=True
+)
+alarm.exit_and_deep_sleep_until_alarms(button_alarm)
